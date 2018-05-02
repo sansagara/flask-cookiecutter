ce3b598e2dbe8590159f6b94c85c5642a5310022
Guillermo Marin <guillermo.marin@banistmo.com>
2018-05-02 18:39:10 -0500

    Fixed Template.
    Updated cookiecutter templated with new packages and updated readme file.
    
    Cache is commented because PyPI packages is currently out-dated. When using Dockerfile, it breaks.
    
    We need to built a local PyPI repo.

---
+++ banistmo_ms_{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -0,0 +1,60 @@
+#! ../env/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = '{{cookiecutter.full_name}}'
+__email__ = '{{cookiecutter.email}}'
+__version__ = '{{cookiecutter.version}}'
+
+from flask import Flask
+from webassets.loaders import PythonLoader as PythonAssetsLoader
+
+from {{cookiecutter.repo_name}}.controllers.main import main
+from {{cookiecutter.repo_name}} import assets
+from {{cookiecutter.repo_name}}.models import db
+
+from {{cookiecutter.repo_name}}.extensions import (
+    # cache,
+    assets_env,
+    debug_toolbar,
+    login_manager
+)
+
+
+def create_app(object_name):
+    """
+    An flask application factory, as explained here:
+    http://flask.pocoo.org/docs/patterns/appfactories/
+
+    Arguments:
+        object_name: the python path of the config object,
+                     e.g. {{cookiecutter.repo_name}}.settings.ProdConfig
+
+        env: The name of the current environment, e.g. prod or dev
+    """
+
+    app = Flask(__name__)
+
+    app.config.from_object(object_name)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # initialize the cache
+    # cache.init_app(app)
+
+    # initialize the debug tool bar
+    debug_toolbar.init_app(app)
+
+    # initialize SQLAlchemy
+    db.init_app(app)
+
+    login_manager.init_app(app)
+
+    # Import and register the different asset bundles
+    assets_env.init_app(app)
+    assets_loader = PythonAssetsLoader(assets)
+    for name, bundle in assets_loader.load_bundles().items():
+        assets_env.register(name, bundle)
+
+    # register our blueprints
+    app.register_blueprint(main)
+
+    return app
